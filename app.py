61942ec01f23656484263fbf8a1490d6db8f5302
Nitin <nitin@Nitins-MacBook-Air.local>
2025-04-14 13:09:03 +0530

    Make app.py exit after printing metrics

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ if __name__ == "__main__":
     while True:
         print(get_metrics())
         time.sleep(5)
+
+
+
+print("✅ CPU Usage:", psutil.cpu_percent())
+print("✅ Memory Usage:", psutil.virtual_memory().percent)
+print("✅ Disk Usage:", psutil.disk_usage('/').percent)
